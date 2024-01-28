7206afba3f903d58aa8e39252ef102ba812e04d1
rjspyk5 <rjspyk5@gmail.com>
2024-01-28 18:51:54 +0600

    Toast message added

--- Background-change-with-hex/index.js
+++ Background-change-with-hex/index.js
@@ -1,9 +1,21 @@
 const input = document.querySelectorAll(".input")[0];
 const btn = document.querySelectorAll(".btn")[0];
 const copy = document.querySelectorAll(".copy")[0];
+const parent = document.querySelectorAll(".parent")[0];
+
+const genarateTextMessage = () => {
+  const div = document.createElement("div");
+  div.style.position = "fixed";
+  div.style.top = "30%";
+  div.style.left = "47%";
+
+  div.innerText = "Text copied";
+  document.body.appendChild(div);
+};
 
 const copyText = () => {
   navigator.clipboard.writeText(`${input.value}`);
+  genarateTextMessage();
 };
 
 const changeBg = () => {
