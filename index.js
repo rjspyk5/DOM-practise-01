a6728fcc91a9cec2f3c5dbaf7f5a1f72805e4dd5
rjspyk5 <rjspyk5@gmail.com>
2024-01-28 13:36:36 +0600

    Complete

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const changeBg=()=>{
-    document.body.style.backgroundColor="red"
+   
+    const r=Math.floor(Math.random()*255)
+    const g=Math.floor(Math.random()*255)
+    const b=Math.floor(Math.random()*255)
+    const color=`rgb(${r},${g},${b})`
+    document.body.style.backgroundColor=color
  }
 const btn=document.querySelector(".btn")
 btn.addEventListener("click",changeBg)
