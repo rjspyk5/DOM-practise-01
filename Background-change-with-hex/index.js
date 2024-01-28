6698a486c355b8f10d6955e8ac3ae917644b0479
rjspyk5 <rjspyk5@gmail.com>
2024-01-28 18:31:11 +0600

    Copy button and functionlaity added

--- Background-change-with-hex/index.js
+++ Background-change-with-hex/index.js
@@ -1,13 +1,19 @@
 const input = document.querySelectorAll(".input")[0];
 const btn = document.querySelectorAll(".btn")[0];
+const copy = document.querySelectorAll(".copy")[0];
+
+const copyText = () => {
+  navigator.clipboard.writeText(`${input.value}`);
+};
 
 const changeBg = () => {
   const r = Math.floor(Math.random() * 255).toString(16);
   const g = Math.floor(Math.random() * 255).toString(16);
   const b = Math.floor(Math.random() * 255).toString(16);
   const hexcolor = `#${r}${g}${b}`;
-  document.body.style.color = `#${r}${g}${b}`;
   document.body.style.backgroundColor = `#${r}${g}${b}`;
   input.value = hexcolor;
 };
+
 btn.addEventListener("click", changeBg);
+copy.addEventListener("click", copyText);
